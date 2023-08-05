c14579ab17e8c4fd2a6783fb5a8861cd856fd575
Navkiran Singh <navkiran054@gmail.com>
2023-08-05 17:14:31 -0400

    my wifi complete

---
+++ notes.rs
@@ -0,0 +1,13 @@
+notes
+
+onclick="window.location.href='___';" 
+
+onclick="window.location.href='Merckuweb_dashboard_device_limit_time.html';"
+onclick="window.location.href='Merckuweb_dashboard_device_limit_speed.html';"
+onclick="window.location.href='Merckuweb_dashboard_device_limit_url.html';" 
+
+
+
+use selected 
+
+  
