33f87634eccd926f3e2e94460f41e77d60391f97
Navkiran Singh <navkiran054@gmail.com>
2023-08-05 21:18:43 -0400

    complete

--- notes.rs
+++ notes.rs
@@ -7,7 +7,38 @@ onclick="window.location.href='Merckuweb_dashboard_device_limit_speed.html';"
 onclick="window.location.href='Merckuweb_dashboard_device_limit_url.html';" 
 
 
+onclick="window.location.href='Merckuweb_advance_portforwarding_add.html';" 
 
-use selected 
 
-  
+onclick="window.location.href='Merckuweb_dashboard.html';" 
+onclick="window.location.href='Merckuweb_setting_wifi.html';" 
+onclick="window.location.href='Merckuweb_advance_portforwarding.html';" 
+onclick="window.location.href='Merckuweb_upgrade_online.html';" 
+onclick="window.location.href='Merckuweb_theme.html';" 
+
+
+                <li onclick="window.location.href='Merckuweb_dashboard.html';" data-v-48b507fe="" class="nav-item">
+                    <div   data-v-48b507fe="" data-title="My WiFi" class="nav-item-content">
+                      <i data-v-48b507fe="" class="el-menu-item__icon iconfont icon-ic_home_light1"></i>
+                    </div>
+                  </li>
+                  <li onclick="window.location.href='Merckuweb_setting_wifi.html';"  data-v-48b507fe="" class="nav-item">
+                    <div data-v-48b507fe="" data-title="Settings" class="nav-item-content">
+                      <i data-v-48b507fe="" class="el-menu-item__icon iconfont icon-ic_home_settings_light1"></i>
+                    </div>
+                  </li>
+                  <li onclick="window.location.href='Merckuweb_advance_portforwarding.html';"   data-v-48b507fe="" class="nav-item">
+                    <div data-v-48b507fe="" data-title="Advanced Settings" class="nav-item-content">
+                      <i data-v-48b507fe="" class="el-menu-item__icon iconfont icon-ic_advanced_settings_light"></i>
+                    </div>
+                  </li>
+                  <li onclick="window.location.href='Merckuweb_upgrade_online.html';"  data-v-48b507fe="" class="nav-item selected">
+                    <div data-v-48b507fe="" data-title="Upgrade Firmware" class="nav-item-content">
+                      <i data-v-48b507fe="" class="el-menu-item__icon iconfont icon-ic_upgrade_firmware_selected_light"></i>
+                    </div>
+                  </li>
+                  <li onclick="window.location.href='Merckuweb_theme.html';"  data-v-48b507fe="" class="nav-item">
+                    <div data-v-48b507fe="" data-title="Theme" class="nav-item-content">
+                      <i data-v-48b507fe="" class="el-menu-item__icon iconfont icon-ic_theme_light"></i>
+                    </div>
+                  </li>
